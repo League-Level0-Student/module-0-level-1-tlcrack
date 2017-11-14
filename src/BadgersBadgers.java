1e85a07985c8e3855533c81f3dd52e89aefb6ab0
Sofisa <league@WorthyMini-6.attlocal.net>
2017-11-13 18:22:20 -0800

    finishModule1

---
+++ src/BadgersBadgers.java
@@ -0,0 +1,21 @@
+
+public class BadgersBadgers {
+	public static void main(String[] args) {
+		
+		for (int i = 0;i < 3; i++ ) {	
+for (int b = 0;b < 10; b++ ) {
+		System.out.print("badger, ");
+}     
+System.out.println("");
+for (int m = 0;m < 2; m++ ) {
+	System.out.print("mushroom, ");
+}    
+System.out.print("a");
+System.out.println("");
+		}
+	}  
+
+
+	
+
+}
